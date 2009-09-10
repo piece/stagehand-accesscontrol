519751cd2280566d9c1e6367e760ca9bd167ed62
KUBO Atsuhiro <kubo@iteman.jp>
2009-09-11 02:38:07 +0900

    Added the tests directory into the include_path.

--- tests/prepare.php
+++ tests/prepare.php
@@ -37,7 +37,8 @@
 
 error_reporting(E_ALL | E_STRICT);
 
-set_include_path(realpath(dirname(__FILE__) . '/../src') . PATH_SEPARATOR .
+set_include_path(realpath(dirname(__FILE__)) . PATH_SEPARATOR .
+                 realpath(dirname(__FILE__) . '/../src') . PATH_SEPARATOR .
                  get_include_path()
                  );
 
