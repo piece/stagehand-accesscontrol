28c7beafbed755f6dbe04554f4a0a323f5b776bc
KUBO Atsuhiro <kubo@iteman.jp>
2009-09-11 02:59:50 +0900

    Revert "Added the tests directory into the include_path."
    This reverts commit 519751cd2280566d9c1e6367e760ca9bd167ed62.

--- tests/prepare.php
+++ tests/prepare.php
@@ -37,8 +37,7 @@
 
 error_reporting(E_ALL | E_STRICT);
 
-set_include_path(realpath(dirname(__FILE__)) . PATH_SEPARATOR .
-                 realpath(dirname(__FILE__) . '/../src') . PATH_SEPARATOR .
+set_include_path(realpath(dirname(__FILE__) . '/../src') . PATH_SEPARATOR .
                  get_include_path()
                  );
 
